989ce0c85bfbe167777dc2a84e3c2d3f7bb5eada
paulbellmann <paul.bellmann@yahoo.de>
2019-12-02 23:25:44 +0100

    add part 2

--- day01/part02.py
+++ day01/part02.py
@@ -1,48 +1,21 @@
-import argparse
-import pytest
-
-"""
-Fuel required to launch a given module is based on its mass. Specifically,
-to find the fuel required for a module, take its mass, divide by three,
-round down, and subtract 2.
-
-The Fuel Counter-Upper needs to know the total fuel requirement.
-To find it, individually calculate the fuel needed for the mass of 
-each module (your puzzle input), then add together all the fuel values.
-"""
-def compute(s: str) -> int:
+def calculate_fuel(data) -> int:
     total = 0
-    for line in s.splitlines():
-        temp = (int(line) // 3) - 2
-        total += temp
 
-    compute(str(total))
+    for line in data:
+        mass = int(line)
+        fuel = mass // 3 - 2
+        total += fuel
+
+        while ((fuel // 3 - 2) > 0):
+            fuel = fuel // 3 - 2
+            total += fuel
 
     return total
 
 
-@pytest.mark.parametrize(
-    ('input_s', 'expected'),
-    (
-        ('14', 2),
-        ('1969', 50346),
-        ('1969', 654),
-        ('100756', 33583),
-    ),
-)
-def test(input_s: str, expected: int) -> None:
-    assert compute(input_s) == expected
+def main() -> None:
+    with open('input.txt') as file:
+        print(calculate_fuel(file))
 
-def main() -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--data_file')
-    args = parser.parse_args()
-
-    with open(args.data_file) as f:
-        print(compute(f.read()))
-
-    return 0
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
